8a7c8eb004da2428477c88382015450f6c732e4e
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-07 22:51:11 +0530

    Added output to trial.py

--- trial.py
+++ trial.py
@@ -1,6 +1,8 @@
-# for x in range(10):
-#     for y in range(x):
-#         print(y)
-
-for x in range(1, 10, 3):
-    print(x)
+count = 0
+for x in range(10):
+    for y in range(x): # 0 0 0 1 0 1 2 0 1 2 3 0 1 2 3 4 0 1 2 3 4 5 0 1 2 3 4 5 6 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 8 0 1 2 3 4 5 6 7 8 9 
+        # print(y)
+        count+=1
+print(count)
+# for x in range(1, 10, 3):
+#     print(x)
