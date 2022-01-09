224a14de9fdae9acdadf589552534dafbb6c15e3
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-09 12:06:32 +0530

    Added logic breakdown for multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -16,3 +16,12 @@ multiplication_table(1, 3)
 
 # 3 6 9
 
+# multiplication table logic breakdown
+
+# Function Definiton: multiplication_table	|	parameter value: start, stop
+# for loop	|	using range function starting from start and ending at stop+1
+# for loop	|	using range function starting from start and ending at stop+1
+# printing string x * y for for loop of y
+# printing function for for loop of x
+
+# puuting value of 1,3 in the function which means, start=1, stop=3
