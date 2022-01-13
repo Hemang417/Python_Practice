6124f0ea4dd2b5c62aebb3679dd9765808a5e83d
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-13 17:44:44 +0530

    Changed the filename
    Changed the filename from .hpp to .h and appended it to newfilenames but cannot retrieve the old file names which are no renamed

---
+++ change_file_name.py
@@ -0,0 +1,11 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = []
+
+for file in filenames:
+    if ".hpp" in file:
+        newfilenames.append(file.replace(".hpp", ".h"))
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
