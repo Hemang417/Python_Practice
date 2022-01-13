9dc4c92ce9d510e2a44ad9a29b7b8b3a912597a2
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-13 16:51:02 +0530

    Skip Elements
    Used .append() to make a new list and prevent breaking of the list of string in characters

---
+++ skip_elements.py
@@ -0,0 +1,18 @@
+def skip_elements(elements):
+	# Initialize variables
+	new_list = []
+	i = 0
+
+	# Iterate through the list
+	for i in range(0, len(elements), 2):
+		# Does this element belong in the resulting list?
+		if elements[i] in elements:
+			# Add this element to the resulting list
+			new_list.append(elements[i])
+		# Increment i
+		# i=i+2
+
+	return new_list
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements([])) # Should be []
