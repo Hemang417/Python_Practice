a5c64d22eede225655b5313b8940b373f94777e7
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-16 23:13:42 +0530

    Added the question in group_per_user

--- group_per_user.py
+++ group_per_user.py
@@ -1,19 +1,25 @@
-def groups_per_user(group_dictionary):
-	user_groups = {}
-	# Go through group_dictionary
-	for group, users in group_dictionary.items():
-		# Now go through the users in the group
-		for user in users:
-			if user not in user_groups:
-				user_groups[user] = []
-			user_groups[user].append(group)
+# The groups_per_user function receives a dictionary, which contains group names with the list of users. 
+# Users can belong to multiple groups. 
+# Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values.
 
-			# Now add the group to the the list of
+
+def groups_per_user(group_dictionary):
+    user_groups = {}
+    # Go through group_dictionary
+    for group, users in group_dictionary.items():
+        # Now go through the users in the group
+        for user in users:
+            if user not in user_groups:
+                user_groups[user] = []
+            user_groups[user].append(group)
+
+            # Now add the group to the the list of
 # groups for this user, creating the entry
 # in the dictionary if necessary
 
-	return(user_groups)
+    return(user_groups)
+
 
 print(groups_per_user({"local": ["admin", "userA"],
-		"public":  ["admin", "userB"],
-		"administrator": ["admin"] }))
+                       "public":  ["admin", "userB"],
+                       "administrator": ["admin"]}))
