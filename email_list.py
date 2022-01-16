cb27ca13c306497303de0b1fe61de10f9a8dcd4f
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-16 23:49:58 +0530

    Added Output

--- email_list.py
+++ email_list.py
@@ -8,3 +8,6 @@ def email_list(domains):
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": [
       "barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+#Output:
+# ['clark.kent@gmail.com', 'diana.prince@gmail.com', 'peter.parker@gmail.com', 'barbara.gordon@yahoo.com', 
+# 'jean.grey@yahoo.com', 'bruce.wayne@hotmail.com']
