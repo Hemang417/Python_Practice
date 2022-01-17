7721ad7c7dcab9fe64af96c84e2c42b17a6caa65
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-17 15:11:44 +0530

    highligh_word
    i think this has a wrong output for second statement

---
+++ highlight_word.py
@@ -0,0 +1,6 @@
+def highlight_word(sentence, word):
+	return " ".join([x.upper() if x.lower() == word.lower() else x for x in sentence.split()])
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
