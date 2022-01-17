95995e3e434dcf9847fb05c15d9aff45be496c98
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-17 15:31:46 +0530

    Solved combine_list.py

--- combine_lists.py
+++ combine_lists.py
@@ -1,9 +1,10 @@
 def combine_lists(list1, list2):
     final_list = []
-    
+    list1 = list1[::-1]
+    final_list.append(list2 + list1)
   # Generate a new list containing the elements of list2
   # Followed by the elements of list1 in reverse order
-
+    return final_list
 
 Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
 Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
