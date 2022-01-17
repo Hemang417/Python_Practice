432e55930b1ce80241201421b7c4eaa3e0de9b61
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-17 15:26:04 +0530

    combine_list intialize py file

---
+++ combine_lists.py
@@ -0,0 +1,11 @@
+def combine_lists(list1, list2):
+    final_list = []
+    
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+
+
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
