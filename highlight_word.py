84fff56b013ed6be7c1711557d569632fb9ab1fa
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-17 15:16:08 +0530

    Added Output

--- highlight_word.py
+++ highlight_word.py
@@ -4,3 +4,8 @@ def highlight_word(sentence, word):
 print(highlight_word("Have a nice day", "nice"))
 print(highlight_word("Shhh, don't be so loud!", "loud"))
 print(highlight_word("Automating with Python is fun", "fun"))
+
+#Output:
+# Have a NICE day
+# Shhh, don't be so loud!
+# Automating with Python is FUN
