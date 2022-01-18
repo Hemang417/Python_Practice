8c07f053687a51df9dfa4049bef736e5b1d299fb
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-18 16:56:34 +0530

    Practice Practice

---
+++ practice.py
@@ -0,0 +1,12 @@
+# even_list = []
+# for item in range(1, 11):
+#     while item % 2 == 0:
+#         even_list.append(item)
+#         break
+# print("Even Numbers: ", even_list)
+
+# adj = ["red", "big", "tasty"] 
+# fruits = ["apple", "banana", "cherry"] 
+# for x in adj: 
+#     for y in fruits: 
+#         print(x, y)
