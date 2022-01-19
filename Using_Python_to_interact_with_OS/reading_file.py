4d7f9dbf57b753e28706bdf50c60e39373e0b4fd
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-20 01:40:42 +0530

    Reading file Dataset added

--- Using_Python_to_interact_with_OS/reading_file.py
+++ Using_Python_to_interact_with_OS/reading_file.py
@@ -1,6 +1,7 @@
 """Here the file_name is the name of the file if the file is in the same directory or you can also enter
 the absolute path of where the file is present"""
-file = open("file_name")
+
+file = open("../README.md")
 
 """This print function consists of a reaedline function which reads line from the file"""
 print(file.readline())
@@ -10,4 +11,3 @@ print(file.readline())
 """If you are executing this read function after executing the readline function then the read function will 
 print the remaining lines present in teh file as it gets incremented to the next line default."""
 print(file.read())
-
