d7080025a5df96a960f8ffca401f3bd43067f855
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-23 11:46:01 +0530

    Adding new guests

--- Using_Python_to_interact_with_OS/reading_and_writing_files.py
+++ Using_Python_to_interact_with_OS/reading_and_writing_files.py
@@ -9,3 +9,15 @@ guests.close()
 with open("guests.txt") as guests:
     for line in guests:
         print(line)
+
+new_guests = ["Sam", "Danielle", "Jacob"]
+
+with open("guests.txt", 'w') as guests:
+    for i in new_guests:
+        guests.write(i + "\n")
+
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
