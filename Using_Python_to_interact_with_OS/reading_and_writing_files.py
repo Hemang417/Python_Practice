e626a0cf061abd05e6ce574989be5fdc45fabfca
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-23 14:46:38 +0530

    done with lab
    Added conditions for check out and to check if the giest are still checked in

--- Using_Python_to_interact_with_OS/reading_and_writing_files.py
+++ Using_Python_to_interact_with_OS/reading_and_writing_files.py
@@ -21,3 +21,43 @@ guests.close()
 with open("guests.txt") as guests:
     for line in guests:
         print(line)
+
+checked_out=["Andrea", "Manuel", "Khalid"]
+temp_list=[]
+
+with open("guests.txt", 'r') as guests:
+    for g in guests:
+        temp_list.append(g.strip())
+
+with open("guests.txt", 'r+') as guests:
+    for name in temp_list:
+        if name not in checked_out:
+            guests.truncate(name + "\n")
+
+checked_out=["Andrea", "Manuel", "Khalid"]
+temp_list=[]
+
+with open("guests.txt", 'r') as guests:
+    for g in guests:
+        temp_list.append(g.strip())
+
+with open("guests.txt", 'w') as guests:
+    for name in temp_list:
+        if name not in checked_out:
+            guests.write(name + "\n")
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+
+guests_to_check = ['Bob', 'Andrea']
+checked_in = []
+
+with open("guests.txt","r") as guests:
+    for g in guests:
+        checked_in.append(g.strip())
+    for check in guests_to_check:
+        if check in checked_in:
+            print("{} is checked in".format(check))
+        else:
+            print("{} is not checked in".format(check))
