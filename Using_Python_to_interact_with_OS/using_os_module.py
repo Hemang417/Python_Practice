20c3a804e057f6ecdd10ecef9a1c7bd72fdaa0cd
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-24 14:52:20 +0530

    Adding docstring to understand the code

--- Using_Python_to_interact_with_OS/using_os_module.py
+++ Using_Python_to_interact_with_OS/using_os_module.py
@@ -1,7 +1,7 @@
 import os
-
+"""This is to remove the file from the path"""
 os.remove("file_name")
-
+"""This is to rename the file from te hold_name to the new_name"""
 os.rename("old_name", "new_name")
 
 os.path.exists("file_name") #if true the file exists
