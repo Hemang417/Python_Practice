29df659ca2787682526c671f706d2a41d25b90aa
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-24 15:34:42 +0530

    Adding docstring to understand the code

--- Using_Python_to_interact_with_OS/using_os_module.py
+++ Using_Python_to_interact_with_OS/using_os_module.py
@@ -3,6 +3,6 @@ import os
 os.remove("file_name")
 """This is to rename the file from te hold_name to the new_name"""
 os.rename("old_name", "new_name")
-
+"""os.path.exists gives boolean value in return as in: True or False"""
 os.path.exists("file_name") #if true the file exists
                             #if false the file does not exist
