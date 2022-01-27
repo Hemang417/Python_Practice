aaf3e76fd2c68eac8097d6b7a3e32fa2d0122f9c
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-27 18:46:06 +0530

    file_date.py

---
+++ Using_Python_to_interact_with_OS/file_date.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open (filename,'w') as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  c=datetime.datetime.fromtimestamp(timestamp)
+  # Convert the timestamp into a readable format, then into a string
+ 
+  # Return just the date portion 
+  # Hint: how many characters are in “yyyy-mm-dd”? 
+  return ("{}".format(c.strftime("%Y-%m-%d")))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
