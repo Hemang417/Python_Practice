bdcd1ba0c9baf0050e5f2bb51d948a5b15810b07
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-27 18:34:19 +0530

    Adding for clear understanding

--- Using_Python_to_interact_with_OS/create_python_script.py
+++ Using_Python_to_interact_with_OS/create_python_script.py
@@ -2,8 +2,8 @@ import os
 def create_python_script(filename):
   comments = "# Start of a new Python program"
   with open(filename, "w") as file:
-    file.write(comments)
-  filesize = os.path.getsize(filename)
+    file.write(comments) # Writes the sentence present in comments
+  filesize = os.path.getsize(filename) # Gets the size of the file specified
   return(filesize)
 
 print(create_python_script("program.py"))
