00557441b5d27aef35af29ae59caa6ea8afde3ff
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-31 02:01:21 +0530

    csv_script.py working
    Used txt extension instead of .csv extension

--- Using_Python_to_interact_with_OS/csv_script.py
+++ Using_Python_to_interact_with_OS/csv_script.py
@@ -1,9 +1,9 @@
 import csv
 
-f = open("csv_file.txt")
+f = open("hosts.csv")
 csv_f = csv.reader(f)
 
 for row in csv_f:
-    name, phone, role = row
-    print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
+    name, phone = row
+    print("Name: {}, Phone: {}".format(name, phone))
 
