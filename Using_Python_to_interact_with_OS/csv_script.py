9d8a8e2ecf941b97bdf331405c56ea266f28822c
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-31 01:30:44 +0530

    Code not running

--- Using_Python_to_interact_with_OS/csv_script.py
+++ Using_Python_to_interact_with_OS/csv_script.py
@@ -4,6 +4,6 @@ f = open("csv_file.txt")
 csv_f = csv.reader(f)
 
 for row in csv_f:
-    name, phone, role = f
+    name, phone, role = row
     print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
 
