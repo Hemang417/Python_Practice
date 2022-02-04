228be6a08d8ec48d8b522e4972270b1629b5fff1
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-04 22:37:14 +0530

    Adding Explanation

--- Using_Python_to_interact_with_OS/iteratinginfile.py
+++ Using_Python_to_interact_with_OS/iteratinginfile.py
@@ -1,3 +1,7 @@
 with open("../README.md") as file:
     for line in file:
         print(line.strip().upper())
+
+"""This piece of code open the file and strips the words of spaces using strip function and capitalises it using upper function"""
+
+"""strip and the upper function are in-built function that are present in python"""
