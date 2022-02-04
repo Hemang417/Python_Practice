f93a69472a8b8de788ff37cb2b020e035dba66a9
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-04 22:32:32 +0530

    Added Comments

--- Using_Python_to_interact_with_OS/csv_script.py
+++ Using_Python_to_interact_with_OS/csv_script.py
@@ -6,4 +6,4 @@ csv_f = csv.reader(f)
 for row in csv_f:
     name, phone = row
     print("Name: {}, Phone: {}".format(name, phone))
-
+"""The Print line prints the naem and phone from each of row from the csv file"""
