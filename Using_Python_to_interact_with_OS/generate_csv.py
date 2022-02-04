2f4905dcdbb7015ea4449bb9d58e43eb2f8dc059
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-04 22:34:26 +0530

    Adding explanation

--- Using_Python_to_interact_with_OS/generate_csv.py
+++ Using_Python_to_interact_with_OS/generate_csv.py
@@ -7,5 +7,6 @@ hosts = [["workstation.local", "192.168.25.46"], ["webserver.local", "10.2.5.6"]
 """hosts contains all the lines that need to go inside the file of the .csv file extension so that we can access and read it later"""
 with open('hosts.csv', 'w') as hosts_csv:
     writer = csv.writer(hosts_csv)
+    """csv.writer writes the list of content present inside the list hosts in an csv file"""
     writer.writerows(hosts)
 
