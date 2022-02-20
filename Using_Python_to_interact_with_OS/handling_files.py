e626e9cffbe1f2ad47babd88e3a01580b4f79764
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-20 10:48:45 +0530

    Module-2: LAB
    Few starting steps to get going and what to expect

---
+++ Using_Python_to_interact_with_OS/handling_files.py
@@ -0,0 +1,7 @@
+"""Modude 2: Lab"""
+
+# Donwload the PEM file
+# Using your linux or macOSX terminal to make the .pem file your session host 
+# chmod 600 <Your .pem file path>
+# using ssh start the session host and connect then the linux instance terminal will open
+# now follow the instructions and code according to the steps mentioned in the lab
