a5ecbff47ad25af34fbd663bb5f7a434efeda528
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-20 10:50:31 +0530

    WEEK-2

--- Using_Python_to_interact_with_OS/handling_files.py
+++ Using_Python_to_interact_with_OS/handling_files.py
@@ -5,3 +5,6 @@
 # chmod 600 <Your .pem file path>
 # using ssh start the session host and connect then the linux instance terminal will open
 # now follow the instructions and code according to the steps mentioned in the lab
+
+"""What to expect"""
+# coding in terminal using nano command (common used in linux)
