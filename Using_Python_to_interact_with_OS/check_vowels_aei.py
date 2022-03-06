15d57e9da6f0deb938f58f11b59559dc2451d0e3
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-03-06 14:07:15 +0530

    CHECK VOWELS aei
    This program is used to check the string containing the vowels aei

---
+++ Using_Python_to_interact_with_OS/check_vowels_aei.py
@@ -0,0 +1,8 @@
+import re
+def check_aei (text):
+  result = re.search(r"a.e.i", text)
+  return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True
