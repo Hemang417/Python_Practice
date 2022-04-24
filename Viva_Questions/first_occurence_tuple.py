d9c4abd77fe9c9ece6aab95a612384c4a03fab6c
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-04-24 23:40:21 +0530

    first_occurence_tuple

---
+++ Viva_Questions/first_occurence_tuple.py
@@ -0,0 +1,11 @@
+test = ("Canada", "India", "Canada", "Japan", "Italy", "Canada")
+ 
+idx = test.index("Japan")
+print(idx)
+ 
+idx = test.index("Canada")
+print(idx)
+ 
+# if "Brazil" in test:
+#     idx = test.index("Brazil")
+#     print(idx)
