491d057462b812f1955b7a3140c8f3f4860b4873
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-04-24 21:26:54 +0530

    find_string_in_list

---
+++ Viva_Questions/find_string_in_list.py
@@ -0,0 +1,12 @@
+# assign list
+l = [1, 2.0, 'have', 'a', 'geeky', 'day']
+
+# assign string
+s = 'day'
+
+# check if string is present in the list
+if s in l:
+	print(f'{s} is present in the list')
+else:
+	print(f'{s} is not present in the list')
+
